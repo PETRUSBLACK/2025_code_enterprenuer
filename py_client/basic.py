b4173a69181c0b503fe97364622f68fd3b3c63b1
PETRUSBLACK <onukwupetrusoge@gmail.com>
2025-02-20 16:29:37 +0100

    studing request

--- py_client/basic.py
+++ py_client/basic.py
@@ -6,6 +6,20 @@ endpoint = "http://127.0.0.1:8000/api"
 
 get_response = requests.get(endpoint, json={"query": "Hello world"})
 print(get_response.text)
+print(request.GET)
+body = request.body
+data = {}
+try:
+    data = json.loads(body)
+except:
+    pass
+
+print(data)
+
+data['params'] = dict(request.GET)
+data['headers'] = dict(request.headers)
+data['content_type'] = request.content_type
+return JsonResponse(data)
 
 # print(get_response.json())
 # print(get_response.status_code)
