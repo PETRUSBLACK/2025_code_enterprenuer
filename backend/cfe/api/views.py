1b9105dd11f326dfe11491c2ad51139f05b14e69
PETRUSBLACK <onukwupetrusoge@gmail.com>
2025-03-07 12:49:52 +0100

    using model_to_data instead of serializer

--- backend/cfe/api/views.py
+++ backend/cfe/api/views.py
@@ -1,13 +1,18 @@
-# import json
-# from django.http import JsonResponse
-# from product.models import Product
+import json
+from django.http import JsonResponse
+from product.models import Product
+from django.forms.models import model_to_dict
 
 def api_home(reuest, *args, **kwargs):
-    pass
-#     model_data = Product.object.all().order_by("?").first()
-#     data = {}
-#     if model_data:
-#         data['title'] = model_data.title
-#         data['content'] = model_data.content
-#         data['price'] = model_data.price
-#     return JsonResponse(data)
+    # pass
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+    return JsonResponse(data)
+        # if model_data:
+    #     data['id'] = model_data.id
+    #     data['title'] = model_data.title
+    #     data['content'] = model_data.content
+    #     data['price'] = model_data.price
+    # return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -5,22 +5,22 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://127.0.0.1:8000/api"
 
-get_response = requests.get(endpoint, json={"query": "Hello world"})
-print(get_response.text)
-print(request.GET)
-body = request.body
-data = {}
-try:
-    data = json.loads(body)
-except:
-    pass
+get_response = requests.get(endpoint, json={"product_id": 123})
+# print(get_response.text)
+# print(request.GET)
+# body = request.body
+# data = {}
+# try:
+#     data = json.loads(body)
+# except:
+#     pass
 
-print(data)
+# print(data)
 
-data['params'] = dict(request.GET)
-data['headers'] = dict(request.headers)
-data['content_type'] = request.content_type
-    return JsonResponse(data)
+# data['params'] = dict(request.GET)
+# data['headers'] = dict(request.headers)
+# data['content_type'] = request.content_type
+#     return JsonResponse(data)
 
-# print(get_response.json())
+print(get_response.json())
 # print(get_response.status_code)
