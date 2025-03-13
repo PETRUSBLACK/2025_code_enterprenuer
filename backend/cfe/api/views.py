31ba83183188edca04c6ca6570f23768e2e259c3
PETRUSBLACK <onukwupetrusoge@gmail.com>
2025-03-13 15:16:50 +0100

    serializer

--- backend/cfe/api/views.py
+++ backend/cfe/api/views.py
@@ -1,15 +1,29 @@
 import json
-from django.http import JsonResponse
+# from django.http import JsonResponse
 from product.models import Product
 from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from product.serializers import ProductSerializers
 
+@api_view()
 def api_home(reuest, *args, **kwargs):
-    # pass
-    model_data = Product.objects.all().order_by("?").first()
+    """
+    DRF API View
+    """
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
-    return JsonResponse(data)
+    print("yes")
+    if instance:
+        data = ProductSerializers(instance).data
+    return Response(data)
+
+    # model_data = Product.objects.all().order_by("?").first()
+    # data = {}
+    # if model_data:
+    #     data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+    # return Response(data)
+    # return JsonResponse(data)
         # if model_data:
     #     data['id'] = model_data.id
     #     data['title'] = model_data.title
