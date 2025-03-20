8ddd2b88367d95ad10ee7a0afcb563031764aa90
PETRUSBLACK <onukwupetrusoge@gmail.com>
2025-03-20 10:52:44 +0100

    perform create and CreateAPIView

--- backend/cfe/product/views.py
+++ backend/cfe/product/views.py
@@ -3,6 +3,22 @@ from rest_framework import generics
 from . models import Product
 from .serializers import ProductSerializers
 
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializers
+    
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        title = serializer.validate_data-get('title')
+        content = serializer.validated_data-get('content') or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
+            
+        print(serializer)
+
+product_create_view = ProductCreateAPIView.as_view()
+
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializers
