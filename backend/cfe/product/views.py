6dc9e1cd5cf5334be8cc752f3ecdd7ed0704363c
PETRUSBLACK <onukwupetrusoge@gmail.com>
2025-03-24 14:27:07 +0100

    function base view

--- backend/cfe/product/views.py
+++ backend/cfe/product/views.py
@@ -1,9 +1,12 @@
 from rest_framework import generics
+from rest_framework.decorators import api_view
 
 from . models import Product
 from .serializers import ProductSerializers
+from django.shortcuts import get_object_or_404
+from django.http import Http404
 
-class ProductCreateAPIView(generics.CreateAPIView):
+class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializers
     
@@ -17,9 +20,37 @@ class ProductCreateAPIView(generics.CreateAPIView):
             
         print(serializer)
 
-product_create_view = ProductCreateAPIView.as_view()
+product_list_create_view = ProductListCreateAPIView.as_view()
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializers
     # lookup_field = 'pk'
+    
+@api_view(['GET', 'POST']) 
+def product_alt_view(request, pk=None, *args, **kwargs):
+    
+    method = request.method
+    
+    if method == "GET":
+        if pk is not None:
+        # get request -> detail view
+            # queryset = Product.objects.filter(pk=pk)
+            # if not queryset.exist():
+            #     raise Http404
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=false).data
+            return Response(data)
+    
+    # list view 
+        queryset = Product.objets.all()
+        data = ProductSerializer(queryset, many=True).data
+        return Response(data)
+    
+    if method == "POST":
+        pass
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            instance = serializer.save()
+            return Response(serializer.data)
+        return Response({"invalid" : "not good data"}, status=500)
