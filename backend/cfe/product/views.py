121f59b4b2902082ec56c9488c4fa697522fd8ff
PETRUSBLACK <onukwupetrusoge@gmail.com>
2025-04-06 21:41:12 +0100

    mixins

--- backend/cfe/product/views.py
+++ backend/cfe/product/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
 from rest_framework.decorators import api_view
 
 from . models import Product
@@ -6,6 +6,32 @@ from .serializers import ProductSerializers
 from django.shortcuts import get_object_or_404
 from django.http import Http404
 from rest_framework.response import Response
+
+class ProductMixinView(
+        mixins.CreateModelMixin,
+        mixins.ListModelMixin,
+        mixins.RetrieveModelMixin,
+        generics.GenericAPIView):
+    
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializers
+    lookup_field = 'pk'
+    
+    def get(self, request, *args, **kwargs):
+        pk = kwargs.get("pk")
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+    def perform_create(self, serializer):
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = "this is a single view doing cool stuff"
+        serializer.save(content=content)
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
