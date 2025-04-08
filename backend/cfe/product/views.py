133b8121159e4a85bdae36ae295f8ccdf2bb8fcc
PETRUSBLACK <onukwupetrusoge@gmail.com>
2025-04-08 13:55:22 +0100

    generic

--- backend/cfe/product/views.py
+++ backend/cfe/product/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, mixins
+from rest_framework import generics, mixins, permissions, authentication
 from rest_framework.decorators import api_view
 
 from . models import Product
@@ -36,6 +36,8 @@ class ProductMixinView(
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializers
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     
     def perform_create(self, serializer):
         # serializer.save(user=self.request.user)
